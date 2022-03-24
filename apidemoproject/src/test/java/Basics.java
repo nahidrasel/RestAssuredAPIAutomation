04f3877a24ad50807ec58765622ba7c5109d0afe
nhdrasel <nhdrasel@gmail.com>
2022-03-25 12:23:15 +1300

    Raw Refactor

--- apidemoproject/src/test/java/Basics.java
+++ apidemoproject/src/test/java/Basics.java
@@ -4,17 +4,20 @@ import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
+
 import files.payload;
 public class Basics {
 	
-	public static void main(String[] args) {
-
-		RestAssured.baseURI="https://rahulshettyacademy.com";
-	
+	public static void main(String[] args) {	
+	//validate if the api is working as expected
+	//add place-> Update Place with New Address-> Get Place to validate if New address is present is response
 	//given = all input details
 	//when = submit the api
 	//then= validate the response
-	
+		
+	RestAssured.baseURI="https://rahulshettyacademy.com";
+		
 	String response= given().log().all().queryParams("key","qaclick123")
 	.header("Content-Type","application/json")
 	.body(payload.AddPlace()) .when().post("maps/api/place/add/json")
@@ -22,12 +25,37 @@ public class Basics {
 	.header("server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
 	
 	System.out.println(response);
-	
 	JsonPath js= new JsonPath(response);//for parsing json
-	
-	String placeId=js.get("palce_id");
+	String placeId=js.getString("place_id");
 	
 	System.out.println(placeId);
+	
+	
+	//update Place
+
+	String newAddress = "Summer Walk, Africa";
+	given().log().all().queryParam("key","qaclick123")
+	.header("Content-Type","application/json")
+	.body("{\r\n"
+			+ "\"place_id\":\""+placeId+"\",\r\n"
+			+ "\"address\":\""+newAddress+"\",\r\n"
+			+ "\"key\":\"qaclick123\"\r\n"
+			+ "}").
+	
+	when().put("maps/api/place/update/json")
+	.then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+	
+	
+	//Get Place 
+	String getPlaceResponse= given().log().all().queryParam("key","qaclick123")
+		.queryParam("place_id",placeId)
+		.when().get("maps/api/place/get/json")
+		.then().assertThat().log().all().statusCode(200).extract().response().asString();
+	
+	JsonPath js1= new JsonPath(getPlaceResponse);
+	String actualAddress= js1.getString("address");
+	System.out.println(actualAddress);
+	Assert.assertEquals(actualAddress, newAddress);
 	}
 }
 	
