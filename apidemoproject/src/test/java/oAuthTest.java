271869312ddf4ab4beaec7d76f9a0939a33c6ae9
nhdrasel <nhdrasel@gmail.com>
2022-03-29 11:03:04 +1300

    Selenium Auto

--- apidemoproject/src/test/java/oAuthTest.java
+++ apidemoproject/src/test/java/oAuthTest.java
@@ -1,18 +1,30 @@
 
 import static io.restassured.RestAssured.*;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.restassured.path.json.JsonPath;
 public class oAuthTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "C:/Users/nahid.mahmud/Downloads/chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php");
+		driver.findElement(By.cssSelector("input[type='email']")).sendKeys("nahid.mahmud.qa");
+		driver.findElement(By.cssSelector("input[type='email']")).sendKeys(Keys.ENTER);
+		Thread.sleep(4000);
+		
+		driver.findElement(By.cssSelector("input[type='password']")).sendKeys("Rasel1997");
+		driver.findElement(By.cssSelector("input[type='password']")).sendKeys(Keys.ENTER);
+		String url= driver.getCurrentUrl();
+		
+		
+		
 		
 		String accessTokenResponse = given()
 		.queryParams("code","")
