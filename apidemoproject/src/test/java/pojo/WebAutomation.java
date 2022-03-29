8ac1b933a093f84c422cfac25e35103b61dfac35
nhdrasel <nhdrasel@gmail.com>
2022-03-29 13:30:49 +1300

    Pojo Class

---
+++ apidemoproject/src/test/java/pojo/WebAutomation.java
@@ -0,0 +1,21 @@
+package pojo;
+
+public class WebAutomation {
+
+	private String courseTitle;
+	private String prices;
+	
+	public String getCourseTitle() {
+		return courseTitle;
+	}
+	public void setCourseTitle(String courseTitle) {
+		this.courseTitle = courseTitle;
+	}
+	public String getPrices() {
+		return prices;
+	}
+	public void setPrices(String prices) {
+		this.prices = prices;
+	}
+	
+}
