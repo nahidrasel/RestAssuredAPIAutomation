0e94856de225d09fe0c974f0e30f684e10106858
nhdrasel <nhdrasel@gmail.com>
2022-04-01 10:22:56 +1300

    Static Check

--- APIFramework/src/test/java/resources/Utils.java
+++ APIFramework/src/test/java/resources/Utils.java
@@ -13,15 +13,21 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 public class Utils {
-	RequestSpecification req;
+	
+	public static RequestSpecification req;
 	public RequestSpecification requestSpecification() throws IOException 
+	
 	{
+		if(req==null)
+		{
 		PrintStream log= new PrintStream(new FileOutputStream("logging.text"));
 		 req= new RequestSpecBuilder().setBaseUri(getGlobalValue("baseUrl")).addQueryParam("key","qaclick123")
 				.addFilter(RequestLoggingFilter.logRequestTo(log))
 				.addFilter(ResponseLoggingFilter.logResponseTo(log))
 				.setContentType(ContentType.JSON).build();
 		 return req;
+	}
+		return req;
 	}
 	public static String getGlobalValue(String key) throws IOException
 	{
