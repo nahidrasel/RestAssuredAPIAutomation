5e87cf20afbf1e4feaeba82fc5f0fb9b3d79b3a1
nhdrasel <nhdrasel@gmail.com>
2022-04-04 11:22:36 +1200

    Tags and Hooks

--- APIFramework/src/test/java/features/placeValidation.feature
+++ APIFramework/src/test/java/features/placeValidation.feature
@@ -1,13 +1,22 @@
 Feature: Validating Place API's
 
-Scenario Outline: Verify if Place is being Successsfully added using AddPlaceAPI
+  @AddPlace
+  Scenario Outline: Verify if Place is being Successfully added using AddPlaceAPI
     Given Add Place Payload with "<name>" "<language>" "<address>"
-    When user calls "addPlaceAPI" with "Post" http request
-    Then the API call is success with status code 200
+    When user calls "AddPlaceAPI" with "POST" http request
+    Then the API call got success with status code 200
     And "status" in response body is "OK"
     And "scope" in response body is "APP"
-    
-Examples: 
-		| name  | language |address           |
-		|AAhouse| English  |World cross center|
-		|BBhouse| Spanish  |Sea cross center  |
+    And verify place_Id created maps to "<name>" using "getPlaceAPI"
+
+    Examples: 
+      | name    | language | address            |
+      | AAhouse | English  | World cross center |
+
+  #|BBhouse| Spanish  |Sea cross center  |
+  @DeletePlace
+  Scenario: Verify if Delete Place functionality is working
+    Given DeletePlace Payload
+    When user calls "deletePlaceAPI" with "POST" http request
+    Then the API call got success with status code 200
+    And "status" in response body is "OK"
--- APIFramework/src/test/java/stepDefinations/StepDefination.java
+++ APIFramework/src/test/java/stepDefinations/StepDefination.java
@@ -23,7 +23,8 @@ public class StepDefination extends Utils {
 	ResponseSpecification resspec;
 	Response response;
 	TestDataBuild data = new TestDataBuild();
-
+	static String place_id;
+	
 	@Given("Add Place Payload with {string} {string} {string}")
 	public void add_Place_Payload_with(String name, String language, String address) throws IOException {
 	    // Write code here that turns the phrase above into concrete actions
@@ -35,31 +36,50 @@ public class StepDefination extends Utils {
 	@When("user calls {string} with {string} http request")
 	public void user_calls_with_http_request(String resource,String method) {
 		
-		APIResources resourceAPI= APIResources.valueOf(resource);
+		APIResources resourceAPI = APIResources.valueOf(resource);
 		System.out.println(resourceAPI.getResource());
 		
 		resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 		
 		if(method.equalsIgnoreCase("POST"))
-		response = res.when().post(resourceAPI.getResource());
+		 response = res.when().post(resourceAPI.getResource());
 		else if(method.equalsIgnoreCase("GET"))
-			response= res.when().get(resourceAPI.getResource());
+		response= res.when().get(resourceAPI.getResource());
 	}
 	
 	
-	@Then("the API call is success with status code {int}")
-	public void the_api_call_is_success_with_status_code(Integer int1) {
+	@Then("the API call got success with status code {int}")
+	public void the_API_call_got_success_with_status_code(Integer int1) {
 	   assertEquals(response.getStatusCode(),200);
 		
 	}
 	@Then("{string} in response body is {string}")
 	public void in_response_body_is(String keyValue, String Expectedvalue) {
-	   String resp= response.asString();
-	   JsonPath js= new JsonPath(resp);
-	   assertEquals(js.get(keyValue).toString(),Expectedvalue);
+	
+	   assertEquals(getJsonPath(response,keyValue),Expectedvalue);
+	}
+	
+	@Then("verify place_Id created maps to {string} using {string}")
+	public void verify_place_id_created_maps_to_using(String expectedName, String resource) throws IOException {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+		//request spec
+		  place_id= getJsonPath(response,"place_id");
+		 res= given().spec(requestSpecification()).queryParam("place_id",place_id );
+		 user_calls_with_http_request(resource,"GET");
+		 	String actualName= getJsonPath(response,"name");
+		 	assertEquals(actualName,expectedName);
+	}
+
+
+	@Given("DeletePlace Payload")
+	public void delete_place_payload() throws IOException {
+	    // Write code here that turns the phrase above into concrete actions
+	   res= given().spec(requestSpecification()).body(data.deletePlacePayload(place_id));
 	}
 
 
 
+	
 
 }
