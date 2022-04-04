30235850a578854f5e1bdbf22c95ed23d713a8ea
nhdrasel <nhdrasel@gmail.com>
2022-04-04 13:15:13 +1200

    Data Driven From Excel

--- ExcelDriven/src/test/java/dataDriven.java
+++ ExcelDriven/src/test/java/dataDriven.java
@@ -1,7 +1,9 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.*;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -20,8 +22,41 @@ public class dataDriven {
 			if(workbook.getSheetName(i).equalsIgnoreCase("testdata"))
 			{
 			XSSFSheet sheet= workbook.getSheetAt(i);
+			Iterator <Row> rows = sheet.rowIterator();//sheet is collection of rows
+			Row firstrow= rows.next();
+			Iterator<Cell> ce = firstrow.cellIterator();//row is collection of cells
+			int k=0;
+			int coloumn = 0;
+		while(ce.hasNext())
+		{
+				Cell value= ce.next();
+				if(value.getStringCellValue().equalsIgnoreCase("TestCases"))
+				{
+					coloumn=k;
+				}
+				k++;
 			}
-	}
+		System.out.println(coloumn);
+		
+		//rows counts for the column
+		
+		while(rows.hasNext())
+		{
+			Row r= rows.next();
+			if(r.getCell(coloumn).getStringCellValue().equalsIgnoreCase("Purchase"))
+			{
+				//after grap pull all the data
+				Iterator<Cell> cv= r.cellIterator();
+				while(cv.hasNext())
+				{
+					System.out.println(cv.next().getStringCellValue()) ;
+				}
 
+			}
+			
+		}
+		
+}
+		}
 	}
 }
